86e109b0e126896128f05c3f4efa447820d04585
PramukhNadig <pnadig1@gmail.com>
2022-02-09 04:17:33 -0500

    Initial Commit!

---
+++ main.py
@@ -0,0 +1,5 @@
+##############################################################
+# Create rankings based on usage of word using sigmoid function
+# Use letters and somehow allow for duplicate letters, (maybe don't allow for duplicate letters until search space is small enough)
+# Get list of words lol
+#figure out GUI
