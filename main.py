1eeb6801bb969decf410c4ba7346f569064f0cca
PramukhNadig <pnadig1@gmail.com>
2022-02-09 04:50:13 -0500

    Frequency Word File Cleaned

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
+#!/bin/python3
 ##############################################################
 # Create rankings based on usage of word using sigmoid function
 # Use letters and somehow allow for duplicate letters, (maybe don't allow for duplicate letters until search space is small enough)
 # Get list of words lol
 #figure out GUI
+
+def setup():
+    freq = open('words.txt', 'r+')
+    lines = freq.readlines()
+    freq.seek(0)
+    print(len(lines))
+    count = 0
+    for line in lines:
+        line = str(line)
+        l = line.split(' ')
+        if(len(l[0]) == 5):
+            freq.write(line)
+    freq.truncate()
+    freq.close()
+
+def main():
+    setup()
+if __name__ == "__main__":
+    main()
