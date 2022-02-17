c560e48df3049eb8258106020f1d4c09e55d64b4
PramukhNadig <pnadig1@gmail.com>
2022-02-17 02:53:33 -0500

    Duplicates handled for the most part, 5/6 today tho 4th letter was terrible :(

--- main.py
+++ main.py
@@ -4,10 +4,13 @@
 # Use letters and somehow allow for duplicate letters, (maybe don't allow for duplicate letters until search space is small enough)
 # Get list of words lol
 #figure out GUI
+
+#tweak takeoutletterG such that duplicates are handled.
 import math
 import itertools
-
+from webbrowser import get
 wordsAndFreq = {}
+lettersInWord = set()
 class Color():
     GREEN=2
     YELLOW=1
@@ -37,16 +40,23 @@ def countWords():
         wordsAndFreq[l[0]] = int(l[1].strip())
     print(wordsAndFreq.get("first"))
 
-def takeOutLetterG(letter):
+def takeOutLetterG(letter):        
     for word in wordsAndFreq.copy().keys():
         if(letter in word):
             del wordsAndFreq[word]
 
 def takeOutLetter(letter, position):
     position = position -1
-    for word in wordsAndFreq.copy().keys():
-        if(not(word[position] == letter)):
-            del wordsAndFreq[word]
+    if((letter == 'l') and (position == 3)):
+        print(letter)
+    if(letter not in lettersInWord):
+        takeOutLetterG(letter)
+    else:
+        for word in wordsAndFreq.copy().keys():
+            if(word == 'caulk'):
+                print(":(")
+            if((word[position] == letter)):
+                del wordsAndFreq[word]
 
 def takeOutLetterY(letter, position):
     position = position -1
@@ -61,24 +71,19 @@ def takeOutLetterGR(letter, position):
             del wordsAndFreq[word]
 
 def getWordArray(word, colors):
+    for i in range(len(colors)):
+        if(colors[i] == 1 or colors[i] == 2):
+            lettersInWord.add(word[i])
     for i in range(len(word)):
         if(colors[i] == Color.GREEN):
             takeOutLetterGR(word[i], i+1)
         elif(colors[i] == Color.GRAY):
-            takeOutLetterG(word[i])
+            takeOutLetter(word[i], i+1)
         elif(colors[i] == Color.YELLOW):
             takeOutLetterY(word[i], i+1)
     return wordsAndFreq
 
-#take letter, remove all words that dont match from copy and return remaining size over total.
-def calculateLikelihood(word, colors, arr):
-    total = len(arr)
-    arr1 = arr.keys().copy()
-    for i in range(len(word)):
-        if(colors[i] == Color.GREEN):
-            for word in arr1.keys():
-                if(not(word[i] == word[i])):
-                    del wordsAndFreq[word]
+
 
 
 def getAllPermutationsColors():
@@ -93,19 +98,12 @@ def getAllPermutationsColors():
                         arr.append([i, j, k, l, m])
     return arr
 
-def main():
-    arr = getAllPermutationsColors()
-    print(arr)
-    setup()
-    countWords()
-    getWordArray('smart', arr[220])
-    getWordArray('smrek', [2, 2, 1, 0, 0])
-
 
 def main():
     setup()
     countWords()
-    getWordArray('smart', [Color.YELLOW, Color.GRAY, Color.GRAY, Color.GRAY, Color.YELLOW])
+    print(len(getAllPermutationsColors()))
+    getWordArray('penis', [0,0,0,0,2])
     sorted(wordsAndFreq.keys())
     print(wordsAndFreq)
 
